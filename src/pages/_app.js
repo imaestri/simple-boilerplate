069c44c5c608b0690a16649d389179fb1d8de217
imaestri <isaquemaestri01@gmail.com>
2021-12-23 19:27:14 -0300

    config rtl and jest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import GlobalStyles from '../styles/global'
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Simple Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+App.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object
+}
+
+export default App
